ed7b90b38768b77da241fcb6f021b019284dc358
Joshua Karns <jkarns275@gmail.com>
2019-06-20 14:12:55 -0400

    Cleaning up the API and code

--- lineparser.pyx
+++ lineparser.pyx
@@ -1,4 +1,5 @@
-#cython: boundscheck=False, nonecheck=False, wraparound=False, cdivision=True
+# -*- coding: utf-8 -*-
+#cython: boundscheck=False, nonecheck=False, wraparound=False, cdivision=True, language_level=3
 from libc.stdlib cimport malloc, free, strtol, strtod
 from libc.stdio cimport fseek, fopen, fclose, ferror, ftell, fread, SEEK_END, SEEK_SET, FILE, printf
 from libc.string cimport strncpy, strerror
@@ -27,7 +28,7 @@ cdef inline int parse_f64(void *output, const char *str, long line_n, int field_
     if endptr - str < field_len:
         c = endptr[0]
         # If the parser ended on something other than a space, there is probably an issue
-        if c not in [' ', '\n', '\r']:
+        if c not in [b' ', b'\n', b'\r']:
             return 1
     errno = prev
     return 0
@@ -46,7 +47,7 @@ cdef inline int parse_i64(void *output, const char *st, long line_n, int field_l
     if endptr - st < field_len:
         c = endptr[0]
         # If the parser ended on something other than a space, there is probably an issue
-        if c not in [' ', '\n', '\r']:
+        if c not in [b' ', b'\n', b'\r']:
             return 1
     errno = prev
     return 0
@@ -67,193 +68,30 @@ ctypedef int (*ParseFn)(void *, const char *, long, int)
 
 cdef ParseFn* parse_fn_map = [parse_f64, parse_i64, parse_string]
 
-ctypedef struct Field:
+ctypedef struct CField:
     Ty ty
     int len
-
-ctypedef struct MLErr:
-    char *line
-    long line_n
-
-ctypedef struct MLOk:
-    char **lines
-    long nlines
-
-cdef union MLResult:
-    MLOk ok
-    MLErr err
-
-ctypedef struct MakeLinesResult:
-    int err
-    MLResult res    
-
-cdef char* copy_string(char* src, long len):
-    cdef char *str = <char *> malloc(len + 1)
-    str[len] = 0
-    strncpy(str, src, len)
-    return str
-
-cdef MakeLinesResult make_bad_line_err(char *line, long len, int line_n):
-    cdef MLErr err
-    err.line_n = line_n
-    err.line = copy_string(line, len)
-    
-    cdef MakeLinesResult res
-    res.err = BAD_LINE
-    res.res.err = err
-    
-    return res
-
-cdef MakeLinesResult make_ok(char **lines, long nlines):
-    cdef MLOk ok
-    ok.lines = lines
-    ok.nlines = nlines
-    
-    cdef MakeLinesResult res
-    res.res.ok = ok
-    res.err = 0
-
-    return res
-
-cdef MakeLinesResult make_err(int error):
-    cdef MLErr err
-    err.line_n = -1
-    err.line = NULL
-    
-    cdef MakeLinesResult res
-    res.res.err = err
-    res.err = error
-    
-    return res
-
-cdef char CR = 0x0D # '\r'
-cdef char LF = 0x0A # '\n'
-cdef MakeLinesResult make_lines(Field *fields, int nfields, char *data, long data_len):
-    cdef MakeLinesResult res
-    cdef MLErr err
-    cdef MLOk ok
-
-    cdef long expected_line_len = 0
-    cdef int i = 0
-
-    while i < nfields:
-        expected_line_len += fields[i].len
-        i += 1
-
-    if expected_line_len <= 0:
-        return make_err(BAD_FIELDS)
-
-    cdef long line_n = 0
-    cdef long len
-    cdef char **lines = <char **> malloc(sizeof(char *) * (data_len / expected_line_len))
-    cdef char *str = data
-    cdef char c = 0
-    cdef char *current_line_head = str
-
-    if lines == NULL:
-        return make_err(OUT_OF_MEMORY)
-    while True:
-        c = str[0]
-        
-        if c == CR or c == LF:
-            len = <long> (str - current_line_head)
-            
-            if len != expected_line_len:
-                free(lines)
-                return make_bad_line_err(current_line_head, len, line_n)
-            
-            str += 1
-            
-            while True:
-                c = str[0]
-                if c == CR or c == LF:
-                    str += 1
-                    continue
-                elif c == 0:
-                    lines[line_n] = current_line_head
-                    line_n += 1         
-                    return make_ok(lines, line_n)
-                else:
-                    lines[line_n] = current_line_head
-                    current_line_head = str
-                    line_n += 1
-                    break
-            
-            continue
-
-        elif c == 0:
-            len = <long> (str - current_line_head)
-            if len != expected_line_len:
-                free(lines)
-                return make_bad_line_err(current_line_head, len, line_n)
-            
-            lines[line_n] = current_line_head
-            line_n += 1
-
-            return make_ok(lines, line_n)
-        else:
-            str += 1 
-
-    # Unreachable
-
-ctypedef struct ParsedResult:
-    long line_n
-    long field_index
-
-cdef ParsedResult _parse(char **lines, long nlines, Field *fields, void **output, long nfields):
-    cdef ParsedResult pr
-    cdef long i = 0
-    cdef int j, ty = 0, length = 0
-    cdef char *line
-    cdef char temp
-    cdef int res
-
-    while i < nlines:
-        line = lines[i]
-        j = 0
-
-        while j < nfields:
-            length = fields[j].len
-            temp = line[length]
-            line[length] = 0
-            ty = fields[j].ty
-            
-            if ty == Float64:
-                res = parse_f64(output[j], line, i, j)
-            elif ty == Int64:
-                res = parse_i64(output[j], line, i, j)
-            elif ty == String:
-                res = parse_string(output[j], line, i, j)
-            if res != 0:
-                pr.line_n = i
-                pr.field_index = j
-                return pr
-            
-            line[length] = temp
-            line += length
-            j += 1
-        
-        i += 1
-
-    pr.line_n = -1
-    pr.field_index = -1
-    return pr
 
 ctypedef struct NextLineResult:
     char *line
     int err
 
+
+cdef char LF = 10
+cdef char CR = 13
+
+
 cdef NextLineResult fast_next_line(char* current_position, char* end_position, int line_len):
     cdef char *new_pos = current_position + line_len
     cdef NextLineResult r
+    r.line = NULL
+    r.err = 0
 
     if new_pos >= end_position:
-        r.line = NULL
-        r.err = 0
         return r
 
     cdef char c = new_pos[0]
-    
+
     # Lines should end with newline or null; if not, there is probably a bad line
     if c not in [LF, CR, 0]:
         r.line = NULL
@@ -262,7 +100,7 @@ cdef NextLineResult fast_next_line(char* current_position, char* end_position, i
     new_pos += 1
     while True:
         c = new_pos[0]
-        
+
         if c == LF or c == CR:
             new_pos += 1
         elif c == 0:
@@ -277,6 +115,7 @@ cdef NextLineResult fast_next_line(char* current_position, char* end_position, i
             r.line = new_pos
             return r
 
+
 ctypedef struct FastParseResult:
     # error code
     int err
@@ -287,7 +126,8 @@ ctypedef struct FastParseResult:
     # if there was a parse error, this will have the field index of the error, otherwise -1
     int field_index
 
-cdef FastParseResult _fast_parse(char *data, long data_len, long max_nlines, int line_len, Field *fields, void **output, int nfields):
+
+cdef FastParseResult fast_parse_internal(char *data, long data_len, long max_nlines, int line_len, CField *fields, void **output, int nfields):
     cdef long line_n = 0
     cdef char *end = data + data_len
     cdef char *t = NULL
@@ -297,6 +137,7 @@ cdef FastParseResult _fast_parse(char *data, long data_len, long max_nlines, int
     cdef FastParseResult pr
     cdef NextLineResult nlr
     nlr.line = data
+    nlr.err = 0
 
     while nlr.line != NULL:
         j = 0
@@ -307,7 +148,7 @@ cdef FastParseResult _fast_parse(char *data, long data_len, long max_nlines, int
             temp = nlr.line[length]
             nlr.line[length] = 0
             ty = fields[j].ty
-            
+
             if ty == Float64:
                 res = parse_f64(output[j], t, line_n, fields[j].len)
             elif ty == Int64:
@@ -320,13 +161,13 @@ cdef FastParseResult _fast_parse(char *data, long data_len, long max_nlines, int
                 pr.field_index = j
                 pr.line_n = line_n
                 return pr
-            
+
             j += 1
             nlr.line[length] = temp
 
         nlr = fast_next_line(nlr.line, end, line_len)
         line_n += 1
-    
+
     if nlr.err != 0:
         pr.err = nlr.err
         pr.line_n = line_n
@@ -335,7 +176,7 @@ cdef FastParseResult _fast_parse(char *data, long data_len, long max_nlines, int
         pr.err = 0
         pr.line_n = line_n
         pr.field_index = -1
-    
+
     return pr
 
 ctypedef struct ReadWholeFileResult:
@@ -356,14 +197,15 @@ cdef ReadWholeFileResult read_whole_file(char *path):
     global errno
     cdef int prev = errno
     cdef ReadWholeFileResult r
-    
+
     cdef FILE *f = fopen(path, "rb")
-    
+
     # file couldnt be opened
     if f == NULL:
         r = make_io_err(errno)
         errno = prev
         return r
+
     errno = prev
 
     if fseek(f, 0, SEEK_END) != 0:
@@ -371,7 +213,7 @@ cdef ReadWholeFileResult read_whole_file(char *path):
         fclose(f)
         return r
 
-    cdef long fsize = ftell(f)
+    cdef long fsize = <long> ftell(f)
     if fseek(f, 0, SEEK_SET) != 0:
         r = make_io_err(ferror(f))
         fclose(f)
@@ -387,12 +229,12 @@ cdef ReadWholeFileResult read_whole_file(char *path):
 
     # Either premature EOF or error; almost certainly NOT EOF though since we just checked
     # the length of the file
-    if fread(str, 1, fsize, f) != fsize:
+    if <long> fread(str, 1, fsize, f) != fsize:
         free(str)
         r = make_io_err(ferror(f))
         fclose(f)
         return r
-    
+
     # Null terminate
     str[fsize] = 0
 
@@ -401,100 +243,6 @@ cdef ReadWholeFileResult read_whole_file(char *path):
     r.data_len = fsize
     r.err = 0
     return r
-
-cdef (Field *, int) make_fields(list fields):
-    """
-    Turns a python list of field tuples into a c array of fields.
-
-    :return: 
-        (NULL, 0) if out of memory
-        (NULL, -1) if there was an issue with the provided field tuples
-        (<ptr>, 0) on success
-    """
-
-    cdef int nfields
-    cdef Field *cfields = NULL
-    cdef int i = 0
-    try:
-        nfields = len(fields)
-        cfields = <Field *> malloc(sizeof(Field) * nfields)
-        
-        # Check if allocation failed
-        if cfields == NULL:
-            return NULL, 0
-        for (ty, length) in fields:
-            if type(length) != int:
-                raise Exception("Expected type int for length, instead got {}".format(type(length)))
-            if type(ty) != Ty:
-                if ty == int:
-                    ty = Int64
-                if ty == float:
-                    ty = Float64
-                if ty == str:
-                    ty = String
-                if type(ty) == int:
-                    if ty > MAX_T or ty < 0:
-                        raise Exception("Invalid ty id {}".format(ty))
-                else:
-                    raise Exception("Expected int or Ty for ty field, instead got {}".format(type(ty)))
-
-            cfields[i].ty = ty
-            cfields[i].len = length
-            i += 1
-        return cfields, nfields
-    except Exception as e:
-        print(f"Got exception {e}")
-        if cfields != NULL:
-            free(cfields)
-            cfields = NULL
-        return cfields, -1
-
-"""
-def t(list pyfields, bytes filename):
-    cdef ReadWholeFileResult file_res = read_whole_file(filename)
-    if file_res.err != 0:
-        return f"Failed to read whole file, encountered error {file_res.err}"
-
-    cdef char *data = file_res.data
-    cdef long data_len = file_res.data_len
-
-    cdef (Field *, int) fields_res = make_fields(pyfields)
-    cdef Field *fields = fields_res[0]
-    cdef int nfields = fields_res[1]
-    if fields == NULL:
-        free(fields)
-        return "Failed to parse fields"
-    if nfields == 0:
-        free(fields)
-        return "Cannot have zero fields"
-
-    cdef int linelen = 0
-    for i in range(nfields):
-        linelen += fields[i].len
-    # cdef MakeLinesResult make_lines(Field *fields, int nfields, char *data, long data_len):
-    cdef MakeLinesResult lines_result = make_lines(fields, nfields, data, data_len)
-
-    cdef char** lines, temp
-    if lines_result.err != 0:
-        return f"Got error {lines_result.err} on line {lines_result.res.err.line_n + 1}"
-    
-    cdef long nlines = lines_result.res.ok.nlines
-
-    lines = lines_result.res.ok.lines
-    cdef AllocationResult output_obj = allocate_field_outputs(fields, nfields, nlines)
-    if output_obj is None:
-        return "Failed to allocate output"
-    cdef void **ptrs = output_obj.ptrs
-    cdef ParsedResult pr = _parse(lines, nlines, fields, output_obj.ptrs, nfields) 
-    if pr.line_n != -1:
-        return "Failed to parse"
-    cdef list py_handles = output_obj.py_handles
-
-    free(lines)
-    free(fields)
-    free(data)
-    return "Ok!"
-"""
 
 class LineParsingError(BaseException):
 
@@ -530,57 +278,150 @@ class LineParsingError(BaseException):
         else:
             return f"error number {self.errno}"
 
-class FieldException(Exception):
-    
+class FieldError(BaseException):
+
     def __init__(self, err):
         self.err = err
 
     def __str__(self):
         return str(self.err)
 
+
+class Field:
+    """
+    Creates a new `Field`, and verifies that the supplied `ty` and `length` parameters are of
+    of the appropriate type.
+
+    Parameters
+    ----------
+    ty : `Ty` or int or `type`
+        The field type. Must be an integer which corresponds to Ty. You can also use the the 'int',
+        'float', and 'str' type classes.
+    length : int
+        The length of the field. This must be a positive integer.
+
+    Examples
+    --------
+    TODO
+    """
+
+    def __init__(self, ty, length):
+        self.ty = self.__check_ty(ty)
+        self.len = self.__check_len(length)
+
+    def __check_ty(self, ty):
+        if type(ty) == Ty:
+            return ty
+
+        if type(ty) == int:
+            if ty > MAX_T or ty < 0:
+                raise FieldError(f"Invalid type id {ty}, type ids must be between 0 and {MAX_T}.")
+            return ty
+
+        if ty == int:
+            return Int64
+        elif ty == float:
+            return Float64
+        elif ty == str:
+            return String
+
+        raise FieldError(f"Invalid type specifier '{ty}'.")
+
+    def __check_len(self, length):
+        if type(length) != int:
+            raise FieldError("Invalid field length type. Field length must be an int.")
+        if length <= 0:
+            raise FieldError("Invalid field length: must be greater than 0.")
+        return length
+
+    def to_cfield(self):
+        cdef CField cf
+        cf.ty = self.ty
+        cf.len = self.len
+        return cf
+
+
 def parse(list pyfields, filename):
-    t = type(filename)
-    if t not in (str, bytes):
-        raise TypeError(f"Argument 'filename' has incorrect type (expected str or bytes, got {t}")
-    cdef ReadWholeFileResult file_res = read_whole_file(bytes(filename))
+    """
+
+    Attempts to parse the lines from `filename` using the field specfications supplied in `pyfields`.
+
+    Parameters
+    ----------
+    pyfields : `list` of Field
+        This list describes the fixed-width file format. The Fields in the list ought to be in the
+        same order that they appear in the file.
+    filename : `str` or `bytes`
+        The filename or path which points to the fixed-width formatted file.
+
+    Returns
+    -------
+    `list` of iterable
+        A list of numpy arrays and lists, where each index in the list corresponds to the field of
+        the same index in the `pyfields` list. For String fields it will be a `list` of `str`, and
+        for Float64 and Int64 it will be a numpy array.
+
+    Raises
+    ------
+    LineParsingError
+        If there is a bad line (wrong length), or a bad field (failed to parse)
+    OSError
+        If this function fails to open `filename`
+    FieldError
+        If there are zero fields provided, or if the provided fields are not all of type `Field`
+    MemoryError
+        If there is not enough memory to read the input file and allocate field containers.
+    """
+    # Ensure fields are properly formatted
+    if len(pyfields) == 0:
+        raise FieldError("Cannot have zero fields.")
+
+    for field in pyfields:
+        if type(field) != Field:
+            raise FieldError("Invalid fields lists. All elements of the list must be of type 'Field'.")
+
+    cdef int nfields = len(pyfields)
+    cdef CField *fields = <CField *> malloc(sizeof(CField) * nfields)
+
+    # Unlikely but theres no reason not to check
+    if fields == NULL:
+        raise MemoryError("There is not enough memory to allocate the fields.")
+
+    # Move them to C structs
+    for i in range(nfields):
+        fields[i] = pyfields[i].to_cfield()
+
+    # Calculate the length of one whole line
+    cdef int linelen = 0
+    for i in range(nfields):
+        linelen += fields[i].len
+
+    if type(filename) not in (str, bytes):
+        raise TypeError(f"Argument 'filename' has incorrect type (expected str or bytes, got {type(filename)}")
+
     cdef char *error = NULL
+    cdef ReadWholeFileResult file_res = read_whole_file(bytes(filename))
+
     if file_res.err != 0:
+        if file_res.err == OUT_OF_MEMORY:
+            raise MemoryError("There is not enough memory to read the entire input file.")
+
         error = strerror(file_res.err)
         raise OSError(file_res.err, str(error))
 
     cdef char *data = file_res.data
     cdef long data_len = file_res.data_len
-
-    cdef (Field *, int) fields_res = make_fields(pyfields)
-    cdef Field *fields = fields_res[0]
-    cdef int nfields = fields_res[1]
-
-    if fields == NULL:
-        free(data)
-        raise FieldException("Supplied fields were not in the proper format.")
-    
-    if nfields == 0:
-        free(fields)
-        free(data)
-        raise FieldException("Cannot have zero fields.")
-
-    cdef int linelen = 0
-    for i in range(nfields):
-        linelen += fields[i].len
-        if fields[i].len == 0:
-            raise FieldException(f"fields[{i}] has a width of zero.")
-        if fields[i].len < 0:
-            raise FieldException(f"fields[{i}] has a width less than zero.")
-
     cdef long max_lines = data_len / linelen
 
     cdef AllocationResult output_obj = allocate_field_outputs(fields, nfields, max_lines)
+
     if output_obj is None:
-        return "Failed to allocate output"
+        raise Exception("Failed to allocate output: out of memory.")
+
     cdef void **ptrs = output_obj.ptrs
     cdef FastParseResult pr = \
-            _fast_parse(data, data_len, max_lines, linelen, fields, output_obj.ptrs, nfields) 
-        
+            fast_parse_internal(data, data_len, max_lines, linelen, fields, output_obj.ptrs, nfields)
+
     cdef int field_pos = -1
     if pr.err != 0:
         field_ty = None
@@ -589,13 +430,13 @@ def parse(list pyfields, filename):
             field_ty = fields[pr.field_index].ty
             for i in range(pr.field_index):
                 field_pos += fields[i].len
-        
+
         free(data)
         free(fields)
         free(ptrs)
-        
+
         raise LineParsingError(pr.err, pr.line_n, field_ty, field_pos, str(filename))
-    
+
     cdef list py_handles = output_obj.py_handles
 
     nlines = pr.line_n
@@ -615,8 +456,8 @@ cdef class AllocationResult:
 
     cdef void **ptrs
     cdef object py_handles
-    
-cdef AllocationResult allocate_field_outputs(const Field *fields, int nfields, long nlines):
+
+cdef AllocationResult allocate_field_outputs(const CField *fields, int nfields, long nlines):
     """
     Allocates output based on the field specifications. The only data type that doesn't get an
     an array for output is string, since c strings don't mix with python very well; so to minimize
